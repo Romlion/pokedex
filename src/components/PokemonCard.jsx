c8f27967ed987fb97005d13514646ab00339ea1b
Romain <98345704+Romlion@users.noreply.github.com>
2023-10-11 15:57:03 +0200

    I added parameter in PokemonCard

---
+++ src/components/PokemonCard.jsx
@@ -0,0 +1,21 @@
+import React from 'react';
+
+const PokemonCard = (props) => {
+  
+const {pokemon} = props
+
+  console.log(props);
+  
+  return (
+    <div>
+        {pokemon.imgSrc !== undefined ? (
+          <img src={pokemon.imgSrc} alt={pokemon.name}/>
+          ) : (
+            <p>???</p>
+            )}
+            {pokemon.name}
+    </div>
+  );
+};
+
+export default PokemonCard;
