d758a5f7ef17bdbfa9b900a0616d5d345a42f91b
Romain <98345704+Romlion@users.noreply.github.com>
2023-10-13 13:40:12 +0200

    I added two functional buttons

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,63 @@
 import './App.css'
 import PokemonCard from './components/PokemonCard'
+import {useState} from "react";
+
 
 function App() {
-const pokemonList = [
-  {
-    name: "bulbasaur",
-    imgSrc:
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
-  },
-  {
-    name: "mew",
-  },
-];
-
-const selectedPokemon = pokemonList[0];
-
-  return (
-    <div>
-      <PokemonCard pokemon={selectedPokemon} />
-  )
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+  
+  const handlePrevious = () => {
+    if(pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    };
   };
+    
+  const handleNext = () => {
+    if (pokemonIndex < pokemonList.length - 1){
+       setPokemonIndex(pokemonIndex + 1);
+    }
+  };
+  
+  return (
+      < section className='app'>
+        <PokemonCard pokemon={pokemonList[pokemonIndex]}/>
+          <div className='navigation'>
+           <button onClick = {handlePrevious} disabled = {pokemonIndex === 0}>Précédent</button>
+           <button onClick = {handleNext} disabled = {pokemonIndex === pokemonList.length - 1}>Suivant</button>
+         </div>
+      </section>
+   );
+  }
 
-export default App
+  const pokemonList = [
+   {
+     name: "bulbasaur",
+     imgSrc:
+    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+    },
+    {
+      name: "charmander",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+    },
+    {
+      name: "squirtle",
+      imgSrc:
+        "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+    },
+    {
+      name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
+    },
+    {
+      name: "mew",
+    },
+  ];
+
+  const selectedPokemon = pokemonList[0];
+    <>
+      <PokemonCard pokemon={selectedPokemon} />
+    </>
+
+export default App;
