c5378c3be7013343f31ffcc78bcca9bb3acbebac
Romain <98345704+Romlion@users.noreply.github.com>
2023-10-21 16:42:55 +0200

    I create component NavBar

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,54 @@
-import './App.css'
-import PokemonCard from './components/PokemonCard'
+import "./App.css"
+import PokemonCard from "./components/PokemonCard"
+import {useState} from "react";
+import NavBar from "./components/NavBar";
+import React from "react";
+
 
 function App() {
+  const [pokemonIndex, setPokemonIndex] = useState(0);
+
+  const handlePrevious = () => {
+    if(pokemonIndex > 0) {
+      setPokemonIndex(pokemonIndex - 1);
+    
+    };
+  };
+
+  const handleNext = () => {
+    if(pokemonIndex < 10) {
+      setPokemonIndex(pokemonIndex + 1);
+    }
+  };
+
+  return (
+    <section className="App">
+      <PokemonCard pokemon ={pokemonList[pokemonIndex]}/>
+      <NavBar handlePrevious={handlePrevious} handleNext={handleNext} />
+    </section>
+    );
+  }
+
 const pokemonList = [
   {
     name: "bulbasaur",
     imgSrc:
-    "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+   "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/1.png",
+  },
+  {
+    name: "charmander",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/4.png",
+  },
+  {
+    name: "squirtle",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/7.png",
+  },
+  {
+    name: "pikachu",
+    imgSrc:
+      "https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/official-artwork/25.png",
   },
   {
     name: "mew",
@@ -14,11 +56,8 @@ const pokemonList = [
 ];
 
 const selectedPokemon = pokemonList[0];
+<>
+<PokemonCard pokemon = {selectedPokemon}/>
+</>
 
-  return (
-    <div>
-      <PokemonCard pokemon={selectedPokemon} />
-  )
-  };
-
-export default App
+export default App;
